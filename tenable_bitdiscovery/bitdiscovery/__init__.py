486ec53909bfb64b0c65db72563fb49381e70b1c
Steven McGrath <steve@chigeek.com>
2020-01-07 12:28:25 -0600

    Updated to support RESTfly 1.1.x changes

--- tenable_bitdiscovery/bitdiscovery/__init__.py
+++ tenable_bitdiscovery/bitdiscovery/__init__.py
@@ -5,7 +5,7 @@ from .inventory import InventoryAPI
 
 class BitDiscovery(APISession):
     _url = 'https://bitdiscovery.com/api/1.0'
-    _vendor = 'Tenable'
+    _vendor = 'BitDiscovery'
     _product = 'BitDiscovery'
     _build = __version__
 
@@ -13,8 +13,8 @@ class BitDiscovery(APISession):
         self._api_key = api_key
         super(BitDiscovery, self).__init__(**kwargs)
 
-    def _build_session(self, session=None):
-        super(BitDiscovery, self)._build_session(session)
+    def _build_session(self, **kwargs):
+        super(BitDiscovery, self)._build_session(**kwargs)
         self._session.headers.update({
             'Authorization': self._api_key
         })
